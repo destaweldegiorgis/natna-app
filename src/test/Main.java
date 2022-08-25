e05093a221fe260ee1d891ea4c39b1a678184dfb
destaweldegiorgis <destaweldegiorgis@gmail.com>
2022-08-25 16:57:10 -0500

    further update on Main class

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,5 +5,7 @@ public class Main {
         System.out.println("Testing1 and Testing 3");
         System.out.println("Testing2");
         System.out.println("Testing3");
+        System.out.println("Testing4");
+        System.out.println("Testing5");
     }
 }
